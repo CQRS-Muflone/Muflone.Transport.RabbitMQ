8b6b0d62bc7920c8ebb890eef21de4a2aadac79e
Alberto Acerbis <alberto.acerbis@gmail.com>
2023-05-05 11:18:02 +0200

    Implemented cnsumer for more than one DomainEvent

---
+++ src/Muflone.Transport.RabbitMQ/Consumers/DomainEventsConsumerBase.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.Extensions.Logging;
+using Muflone.Messages;
+using Muflone.Messages.Events;
+using Muflone.Persistence;
+using Muflone.Transport.RabbitMQ.Abstracts;
+using Muflone.Transport.RabbitMQ.Models;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace Muflone.Transport.RabbitMQ.Consumers;
+
+public abstract class DomainEventsConsumerBase<T> : ConsumerBase, IDomainEventConsumer<T>, IAsyncDisposable
+	where T : class, IDomainEvent
+{
+	private readonly ISerializer _messageSerializer;
+	private readonly IMufloneConnectionFactory _mufloneConnectionFactory;
+	private IModel _channel;
+	private readonly RabbitMQReference _rabbitMQReference;
+
+	public string TopicName { get; }
+
+	protected abstract IEnumerable<IDomainEventHandlerAsync<T>> HandlersAsync { get; }
+
+	protected DomainEventsConsumerBase(RabbitMQReference rabbitMQReference,
+		IMufloneConnectionFactory mufloneConnectionFactory,
+		ILoggerFactory loggerFactory) : base(loggerFactory)
+	{
+		_rabbitMQReference = rabbitMQReference ?? throw new ArgumentNullException(nameof(rabbitMQReference));
+		_mufloneConnectionFactory =
+			mufloneConnectionFactory ?? throw new ArgumentNullException(nameof(mufloneConnectionFactory));
+		_messageSerializer = new Serializer();
+		TopicName = typeof(T).Name;
+	}
+
+	public async Task ConsumeAsync(T message, CancellationToken cancellationToken = default)
+	{
+		foreach (var handlerAsync in HandlersAsync)
+		{
+			await handlerAsync.HandleAsync((dynamic)message, CancellationToken.None);
+		}
+	}
+
+	public Task StartAsync(CancellationToken cancellationToken = default)
+	{
+		InitChannel();
+		InitSubscription();
+
+		return Task.CompletedTask;
+	}
+
+	public Task StopAsync(CancellationToken cancellationToken = default)
+	{
+		StopChannel();
+		return Task.CompletedTask;
+	}
+
+	private void InitChannel()
+	{
+		StopChannel();
+
+		_channel = _mufloneConnectionFactory.CreateChannel();
+
+		Logger.LogInformation(
+			$"initializing retry queue '{TopicName}' on exchange '{_rabbitMQReference.ExchangeEventsName}'...");
+
+		_channel.ExchangeDeclare(_rabbitMQReference.ExchangeEventsName, ExchangeType.Fanout);
+		_channel.QueueDeclare(TopicName, true, false, false);
+		_channel.QueueBind(_rabbitMQReference.QueueEventsName,
+			_rabbitMQReference.ExchangeEventsName,
+			TopicName, //_queueReferences.RoutingKey
+			null);
+
+		_channel.CallbackException += OnChannelException;
+	}
+
+	private void StopChannel()
+	{
+		if (_channel is null)
+			return;
+
+		_channel.CallbackException -= OnChannelException;
+
+		if (_channel.IsOpen)
+			_channel.Close();
+
+		_channel.Dispose();
+		_channel = null;
+	}
+
+	private void OnChannelException(object _, CallbackExceptionEventArgs ea)
+	{
+		Logger.LogError(ea.Exception, "the RabbitMQ Channel has encountered an error: {ExceptionMessage}",
+			ea.Exception.Message);
+
+		InitChannel();
+		InitSubscription();
+	}
+
+	private void InitSubscription()
+	{
+		var consumer = new AsyncEventingBasicConsumer(_channel);
+
+		consumer.Received += OnMessageReceivedAsync;
+
+		Logger.LogInformation($"initializing subscription on queue '{TopicName}' ...");
+		_channel.BasicConsume(TopicName, false, consumer);
+	}
+
+	private async Task OnMessageReceivedAsync(object sender, BasicDeliverEventArgs eventArgs)
+	{
+		var consumer = sender as IBasicConsumer;
+		var channel = consumer?.Model ?? _channel;
+
+		IDomainEvent message;
+		try
+		{
+			message = await _messageSerializer.DeserializeAsync<T>(Encoding.ASCII.GetString(eventArgs.Body.ToArray()),
+				CancellationToken.None);
+		}
+		catch (Exception ex)
+		{
+			Logger.LogError(ex,
+				"an exception has occured while decoding queue message from Exchange '{ExchangeName}', message cannot be parsed. Error: {ExceptionMessage}",
+				eventArgs.Exchange, ex.Message);
+			channel.BasicReject(eventArgs.DeliveryTag, false);
+			return;
+		}
+
+		Logger.LogInformation(
+			"received message '{MessageId}' from Exchange '{ExchangeName}', Queue '{QueueName}'. Processing...",
+			message.MessageId, TopicName, TopicName);
+
+		try
+		{
+			//TODO: provide valid cancellation token
+			await ConsumeAsync((dynamic)message, CancellationToken.None);
+
+			channel.BasicAck(eventArgs.DeliveryTag, false);
+		}
+		catch (Exception ex)
+		{
+			HandleConsumerException(ex, eventArgs, channel, message, false);
+		}
+	}
+
+	private void HandleConsumerException(Exception ex, BasicDeliverEventArgs deliveryProps, IModel channel,
+		IMessage message, bool requeue)
+	{
+		var errorMsg =
+			"an error has occurred while processing Message '{MessageId}' from Exchange '{ExchangeName}' : {ExceptionMessage} . "
+			+ (requeue ? "Reenqueuing..." : "Nacking...");
+
+		Logger.LogWarning(ex, errorMsg, message.MessageId, TopicName, ex.Message);
+
+		if (!requeue)
+		{
+			channel.BasicReject(deliveryProps.DeliveryTag, false);
+		}
+		else
+		{
+			channel.BasicAck(deliveryProps.DeliveryTag, false);
+			channel.BasicPublish(
+				TopicName,
+				deliveryProps.RoutingKey,
+				deliveryProps.BasicProperties,
+				deliveryProps.Body);
+		}
+	}
+
+	#region Dispose
+
+	public ValueTask DisposeAsync()
+	{
+		return ValueTask.CompletedTask;
+	}
+
+	#endregion
+}
