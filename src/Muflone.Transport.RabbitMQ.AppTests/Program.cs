8992d5a8da3848f400b97098f1de0b133ceb71b5
Alberto Acerbis <alberto.acerbis@gmail.com>
2024-08-01 17:38:34 +0200

    Add Application Test

---
+++ src/Muflone.Transport.RabbitMQ.AppTests/Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Muflone;
+using Muflone.Persistence;
+using Muflone.Transport.RabbitMQ;
+using Muflone.Transport.RabbitMQ.Abstracts;
+using Muflone.Transport.RabbitMQ.AppTests;
+using Muflone.Transport.RabbitMQ.Factories;
+using Muflone.Transport.RabbitMQ.Models;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+var rabbitMqConfiguration = new RabbitMQConfiguration("localhost", "guest",
+    "guest", "Muflone.Commands", "Muflone.Events",
+    "Test");
+var mufloneConnectionFactory = new MufloneConnectionFactory(rabbitMqConfiguration, new NullLoggerFactory());
+
+builder.Services.AddMufloneTransportRabbitMQ(new NullLoggerFactory(), rabbitMqConfiguration);
+
+var consumers = new List<IConsumer>
+{
+    new OrderCreatedConsumer(mufloneConnectionFactory, new NullLoggerFactory())
+};
+builder.Services.AddMufloneRabbitMQConsumers(consumers);
+
+
+using IHost host = builder.Build();
+
+PublishEvent(host.Services);
+
+await host.RunAsync();
+
+
+static async Task PublishEvent(IServiceProvider serviceProvider)
+{
+    var eventBus  = serviceProvider.GetRequiredService<IEventBus>();
+    var orderCreated = new OrderCreated(new OrderId(Guid.NewGuid()), "20240801-01");
+
+    await eventBus.PublishAsync(orderCreated, CancellationToken.None);
+}
